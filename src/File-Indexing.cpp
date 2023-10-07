c1cf0a9f823b931ad9c2e2d38cb5e5d2d47ae111
ZURA-Dev <thaplaya03@gmail.com>
2023-10-06 20:59:27 -0700

    removed windows extention files

--- src/File-Indexing.cpp
+++ src/File-Indexing.cpp
@@ -12,6 +12,11 @@
 // ok so read user file and find each offset, then paste that information directly into data file..
 
 // reimplement my main MVP into a more reuseable form
+
+// !! what i need to do is create each file using my fileoperations object, it will be easier to maintain i think.
+// this is because i can just use fileropations close and open files and file stream checkers
+// i also want to learn how pointers and file streams work, and maybe recreate from scratch
+// create support for json files, or create anohter library for indexing and reading json files bc json already has a prebuilt indexers ig
 #include "/home/dev/Documents/projects/ZURA-Lib/File-Operations/src/FileOperations.cpp"
 #include <unordered_map>
 struct IndexEntry
@@ -32,6 +37,18 @@ public:
     fileIndexer(std::string filename) : fileName()
     {
         fileName = filename;
+        createDataFile(fileName);
+    }
+    ~fileIndexer()
+    {
+        if (userFile.is_open())
+        {
+            userFile.close();
+        }
+        if (dataFile.is_open())
+        {
+            dataFile.close();
+        }
     } // user inputs text file
     // openfile fileOperations function
     //
@@ -40,6 +57,7 @@ public:
     // update this to remove anything after a .
     void createDataFile(std::string filename)
     {
+
         std::string searchText = ".txt";
         size_t found = filename.find(searchText);
         while (found != std::string::npos)
@@ -47,7 +65,8 @@ public:
             filename.erase(found, searchText.length());
             found = filename.find(searchText, found);
         }
-        ZURA::fileOperations::s_createFile(filename);
+
+        ZURA::fileOperations::s_createFile(filename + ".data", false); // creates file with .data extention, s_createFile takes care of if file exist and whether to overwrite defualt is false
     }
     void openUserFile()
     {
@@ -71,48 +90,52 @@ public:
 
 int main()
 {
-    /*fileIndexer indexFile; // this is the index file, with the offsets
+    fileIndexer indexfile("testfile.txt");
+    return 0;
+}
+
+/*fileIndexer indexFile; // this is the index file, with the offsets
     fileIndexer dataFile;  // this is just user data file without offsets
     ZURA::fileOperations::s_createFile("indexfile.txt", false);
     indexFile.openfile("indexfile.txt", false);*/
 
-    // MVP prototype, this reads file stream and grabs its offsets, now i need to print these into a file
-    // then search a file for a specific thing like a certain line using the offset
-    std::string filename = "testfile.txt";
-    std::ifstream inputFile(filename);
-    std::vector<std::streampos> lineOffsets;
-    std::string line;
-    lineOffsets.push_back(inputFile.tellg()); // this will get the initial offset of the beginning of the file, so index 0 should be first line of file
-    while (std::getline(inputFile, line))
-    {
-        lineOffsets.push_back(inputFile.tellg());
-    }
-    inputFile.close();
-
-    // printing all offsets
-    for (std::size_t i = 0; i < lineOffsets.size(); ++i)
-    {
-        std::cout << "LINE: " << i << std::endl;
-        std::cout << "OFFSET: " << lineOffsets[i] << std::endl;
-        std::cout << std::endl;
-    }
-    // Search for a specific line number
-    int lineNumberToSearch = 3; // Replace with the desired line number
-    // this only searched THAT line
-    if (lineNumberToSearch >= 1 && lineNumberToSearch <= lineOffsets.size())
-    {
-        // Seek to the offset of the desired line
-        inputFile.open(filename); // Reopen the file
-        inputFile.seekg(lineOffsets[lineNumberToSearch - 1]);
-
-        // Read and print the desired line
-        std::getline(inputFile, line);
-        std::cout << "Line " << lineNumberToSearch << ": " << line << std::endl;
-        inputFile.close();
-    }
-    else
-    {
-        std::cout << "Invalid line number." << std::endl;
-    }
-    return 0;
+// MVP prototype, this reads file stream and grabs its offsets, now i need to print these into a file
+// then search a file for a specific thing like a certain line using the offset
+/*
+std::string filename = "testfile.txt";
+std::ifstream inputFile(filename);
+std::vector<std::streampos> lineOffsets;
+std::string line;
+lineOffsets.push_back(inputFile.tellg()); // this will get the initial offset of the beginning of the file, so index 0 should be first line of file
+while (std::getline(inputFile, line))
+{
+    lineOffsets.push_back(inputFile.tellg());
 }
+inputFile.close();
+
+// printing all offsets
+for (std::size_t i = 0; i < lineOffsets.size(); ++i)
+{
+    std::cout << "LINE: " << i << std::endl;
+    std::cout << "OFFSET: " << lineOffsets[i] << std::endl;
+    std::cout << std::endl;
+}
+// Search for a specific line number
+int lineNumberToSearch = 3; // Replace with the desired line number
+// this only searched THAT line
+if (lineNumberToSearch >= 1 && lineNumberToSearch <= lineOffsets.size())
+{
+    // Seek to the offset of the desired line
+    inputFile.open(filename); // Reopen the file
+    inputFile.seekg(lineOffsets[lineNumberToSearch - 1]);
+
+    // Read and print the desired line
+    std::getline(inputFile, line);
+    std::cout << "Line " << lineNumberToSearch << ": " << line << std::endl;
+    inputFile.close();
+}
+else
+{
+    std::cout << "Invalid line number." << std::endl;
+}
+*/
